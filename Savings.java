639943ef0083f3b958ffc4646be545d7fb5a73ca
KevinII16 <kevinalabi16@gmail.com>
2025-11-03 17:14:24 +0000

    added array and new variable for savings

--- Savings.java
+++ Savings.java
@@ -1,28 +1,31 @@
-public class Savings extends Budget {
-    private double monthly_saving;
+public class Savings extends Budget { // Savings subclass inheriting from the Budget superclass
+    private double savings;
+    private double [] monthly_saving;
     private double savings_account;
     private int months;
 
-    public Savings(double monthly_saving, double savings_account, int months, String name, double total_amount, String continent){
-        super(name,total_amount,continent);
+    public Savings(double [] monthly_saving, double savings_account, int months, String name, double total_amount, String continent,double savings){
+        super(name,total_amount,continent); //calling the superclass constructor
         this.monthly_saving = monthly_saving;
         this.savings_account = savings_account;
         this.months = months;
+        this.savings = savings;
     }
 
     public Savings(){
         super();
-        this.monthly_saving = 0.0;
+        this.monthly_saving = new double[12]; // assuming 12 months of savings
         this.savings_account = 0.0;
         this.months = 0;
+        this.savings = 0.0;
     }
 
-    public double getMonthlySaving(){
+    public double[] getMonthlySaving(){
         return monthly_saving;
     }
 
-    public void setMonthlySaving(double monthly_saving){
-        this.monthly_saving = monthly_saving;
+    public void setMonthlySaving(double[] monthly_saving){ // regular get and set methods for all the variables
+        this.monthly_saving = monthly_saving;   
     }
 
     public double getSavingsAccount(){
@@ -39,5 +42,11 @@ public class Savings extends Budget {
 
     public void setMonths(int months){
         this.months = months;
+    }
+    public double getSavings(){
+        return savings;
+    }
+    public void setSavings(double savings){
+        this.savings = savings;
     }
 }
