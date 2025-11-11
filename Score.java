19e1ad6e0f8468cffb3499fb295c9982a2bb3f9e
X24378823 <x24378823@student.ncirl.ie>
2025-11-11 15:56:44 +0000

    Add files via upload

---
+++ Score.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package quizgame;
+
+/**
+ *
+ * @author RichardBadea
+ */
+
+import javax.swing.JOptionPane;
+
+public class Score extends GuessingGame{
+    private int score;
+    private String[] correctAnswers;
+    
+    public Score(String[] questions, String answers,int score,String[] correctAnswers){
+        super(questions, answers);
+        score = 0;
+        this.correctAnswers = correctAnswers;
+    }
+    
+    public Score(){
+    correctAnswers = new String[5];
+    correctAnswers[0] = "B";
+    correctAnswers[1] = "A";
+    correctAnswers[2] = "D";
+    correctAnswers[3] = "C";
+    correctAnswers[4] = "D";
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+    
+    public void computeScore(String answers,String[] questions, int score, String[] correctAnswers){
+       for(int i = 0; i < questions.length;i++){
+           for(int j = 0; j < correctAnswers.length; j++){
+               if(questions[i] == correctAnswers[j]){
+                   score++;
+               } else if (questions != correctAnswers) {
+                   score--;
+               
+               }
+           } 
+       
+       } if (score == 5) {
+           JOptionPane.showMessageDialog(null,"Good job");
+       }
+        
+    }
+    
+}
